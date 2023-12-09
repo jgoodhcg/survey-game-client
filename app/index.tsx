e9e3d3623541e23dfd83269d0d291383b684d936
Justin <jgoodhcg@gmail.com>
2023-12-09 15:43:52 -0500

    Make landing responsive

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet, Dimensions, Platform } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+const isDesktop = width > 720; // You can define desktop to be any width you deem suitable
 
 export default function LandingPage() {
   return (
-    <View style={styles.container}>
-      <View style={styles.headerContainer}>
+    <View style={[styles.container, isDesktop && styles.containerDesktop]}>
+      <View style={[styles.headerContainer, isDesktop && styles.headerContainerDesktop]}>
         {/* Replace with your logo image */}
-         <Image
+        <Image
           source={require('./../assets/images/adaptive-icon.png')}
-          style={styles.logo}
+          style={[styles.logo, isDesktop && styles.logoDesktop]}
         />
       </View>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button}>
+      <View style={[styles.buttonContainer, isDesktop && styles.buttonContainerDesktop]}>
+        <TouchableOpacity style={[styles.button, isDesktop && styles.buttonDesktop]}>
           <Text style={styles.buttonText}>Start a Game</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.button, styles.buttonOutline]}>
-          <Text style={styles.buttonOutlineText}>Join a Game</Text>
+        <TouchableOpacity style={[styles.button, styles.buttonOutline, isDesktop && styles.buttonDesktop]}>
+          <Text style={[styles.buttonText, styles.buttonOutlineText]}>Join a Game</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -29,16 +31,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
-    backgroundColor: '#fff', // Set your desired background color
+    backgroundColor: '#fff',
+  },
+  containerDesktop: {
+    justifyContent: 'space-around',
   },
   headerContainer: {
     flex: 1,
     justifyContent: 'center',
   },
+  headerContainerDesktop: {
+    flex: 1,
+    justifyContent: 'center',
+  },
   logo: {
-    width: 200, // Set width as per your logo's aspect ratio
-    height: 200, // Set height as per your logo's aspect ratio
-    resizeMode: 'contain', // To ensure the logo is scaled properly
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+  },
+  logoDesktop: {
+    width: 400,
+    height: 400,
+    resizeMode: 'contain',
   },
   buttonContainer: {
     width: '100%',
@@ -46,25 +60,37 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginBottom: 30,
   },
+  buttonContainerDesktop: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    marginBottom: 120,
+  },
   button: {
-    backgroundColor: '#6200ee', // Your primary button color
+    backgroundColor: '#6200ee',
     width: '100%',
     padding: 15,
     borderRadius: 5,
     alignItems: 'center',
     marginBottom: 10,
+    borderColor: '#6200ee',
+    borderWidth: 2,
+  },
+  buttonDesktop: {
+    width: 250, // Fixed width for desktop
+    marginBottom: 0,
+    marginTop: 10,
+    marginRight: 10,
   },
   buttonText: {
-    color: '#ffffff', // Text color for primary button
+    color: '#ffffff',
     fontWeight: 'bold',
   },
   buttonOutline: {
     backgroundColor: '#fff',
-    borderColor: '#6200ee', // Color same as the primary button
+    borderColor: '#6200ee',
     borderWidth: 2,
   },
   buttonOutlineText: {
-    color: '#6200ee', // Color same as the primary button
-    fontWeight: 'bold',
+    color: '#6200ee',
   },
 });
